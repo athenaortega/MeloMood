ae44b4ad785b407ccd1bcb2c1d299d0f57ad2c61
athenaortega <160282703+athenaortega@users.noreply.github.com>
2024-12-06 16:47:15 -0800

    Completed the project, added relevant API, embedded Spotify Playlist, established relevant Flask routes.

--- Final-Project/main.py
+++ Final-Project/main.py
@@ -1,135 +1,68 @@
+from flask import Flask, render_template, request
+import requests
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyClientCredentials
+import keys
 
-"""
-First, import the relevant packages needed for this project.
-My project relies upon spotipy, which is a python library for the Spotify Web API.
-Import all the relevant packages in order to run Flask.
-Import SpotifyOAuth, which creates an access token that allows authenticated calls to the Spotify API
-
-
-Second, import Firebase_admin to access Firebase API for journal entries to be saved in a secure database.
-"""
-import spotipy
-from flask import app, redirect, request, render_template, Flask
-from spotipy.oauth2 import SpotifyOAuth
-
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-
-
-"""
-Create an instance of the Flask app
-Initialize a spotipy object which has access to the Spotify Web API.
-Ensure authentication by using SpotifyOAuth.
-"""
 app = Flask(__name__)
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="0b6e6578e5e543b299468a540559e166",
-                                               client_secret="976090293a504f5f8b328f89dbf61b76", #NEED TO CONCEAL INFO?
-                                               redirect_uri="http://localhost:5000/callback",    #IN SEPARATE FILE?
-                                               scope="user-library-read"))
 
-"""
-Create Flask web app route for what occurs when user reaches the /login URL of MeloMood
-"""
-
-#Sources:
-#https://stackoverflow.com/questions/78440381/spotipy-accessing-users-playlist-access-to-localhost-was-denied-http-error-4
-#https://stackoverflow.com/questions/25711711/spotipy-authorization-code-flow
-@app.route("/login")
-def login():
-    auth_url = sp.auth_manager.get_authorize_url()
-    return redirect(auth_url)
-
-"""
-Create Flask web app route for what occurs when user reaches the /callback URL of MeloMood
-Verify that access token has been obtained.
-Verify that user information has been obtained.
-Obtain 10 of the user's recently played tracks and display name of song, name of artist, and time played.
-Notify user whether authentication has failed or not.
-
-"""
+SPOTIFY_CLIENT_ID = keys.SPOTIFY_CLIENT_ID
+SPOTIFY_CLIENT_SECRET = keys.SPOTIFY_CLIENT_SECRET
 
 
-@app.route("/callback")
-def callback():
-    access_token = None
-    token_info = spotipy.get_cached_token()
+def get_spotify_client():
+    client_credentials_manager = SpotifyClientCredentials(
+        client_id=SPOTIFY_CLIENT_ID,
+        client_secret=SPOTIFY_CLIENT_SECRET
+    )
+    sp = Spotify(client_credentials_manager=client_credentials_manager)
+    return sp
 
-    if token_info:
-        print("Found cached token!")
-        access_token = token_info['access_token']
-    else:
-        code = request.args.get('code')
-        if code:
-            print("Found auth code! Attempting to get a valid access token...")
-            token_info = sp.get_access_token(code)
-            access_token = token_info['access_token']
+CAT_FACTS_API_URL = "https://catfact.ninja/fact"
 
-    if access_token:
-        print("Access token available! Attempting to get user information...")
-        sp = spotipy.Spotify(auth=access_token)
-
-
-        results = sp.current_user_recently_played(limit=10)
-        print("Recently played tracks: ", results)
+def safe_get(url, params=None):
+    try:
+        response = requests.get(url, params=params)
+        return response.json()
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return None
 
 
-        tracks = [
-            {
-                "name": item['track']['name'],
-                "artist": item['track']['artists'][0]['name'],
-                "played_at": item['played_at']
-            }
-            for item in results.get('items', [])
-        ]
-        return render_template('listening.html', tracks=tracks)
 
-    return "Access token not found. Authorization failed."
+def get_cat_facts(limit=5):
+    facts = []
+    for _ in range(limit): #learned about for _ from https://stackoverflow.com/questions/66425508/what-is-the-meaning-of-for-in-range
 
-"""
-Create Flask web app route for the root URL. 
-This route will render the template of the homepage.
+        data = safe_get(CAT_FACTS_API_URL)
+        if data and 'fact' in data:
+            facts.append(data['fact'])
+        else:
+            print("No cat facts found.")
+    return facts
 
-"""
-@app.route("/")
-def home():
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    cat_facts = get_cat_facts(limit=5)
+    playlist_embed_url = None
 
-if __name__ == "__main__":
+    if request.method == 'POST':
+        playlist_keyword = request.form.get('playlist')
+
+
+        sp = get_spotify_client()
+        result = sp.search(q=playlist_keyword, type="playlist", limit=1)
+
+        if result['playlists']['items']:
+            playlist = result['playlists']['items'][0]
+            playlist_url = playlist['external_urls']['spotify']
+            playlist_embed_url = f"https://open.spotify.com/embed/playlist/{playlist['id']}"
+
+
+    return render_template('index.html', cat_facts=cat_facts, playlist_embed_url=playlist_embed_url)
+
+if __name__ == '__main__':
     app.run(debug=True)
 
 
 
-
-
-
-"""
-Setting up Firebase API 
-
-"""
-
-cred = credentials.Certificate("/Users/athenaortega/Documents/HCDE310/Final-Project/venv/melomood-260d9-firebase-adminsdk-qu73i-bf63f51df9.json")
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
-
-@app.route("/")
-def index():
-    return "Firebase Flask App is Running"
-
-"""
-NEXT STEPS:
-
-    1. For each track that is displayed:
-        -> Create a text input box beside the information of track.
-        -> Allow the user to save their journal reflection.
-    2. Save Reflections to Firebase:
-        -> Create function to capture user's reflection text and the song information (title, artist, time played)
-        -> Send POST request to Firebase (send it to endpoint that contains captured data)
-    3. Retrieve Reflections from Firebase:
-        -> When displaying list of songs, must send GET request to Firebase to retrieve saved reflections
-        -> If there are already reflections that exist for a song, allow for the reflection to be displayed next to track
-            as read only text.
-        
- 
-"""
